2129e7f41050a9201ee6171f464e54b9fd048c8d
Akash Ahmed <57281241+Akash-Ahmed-CSE@users.noreply.github.com>
2023-06-10 00:30:42 +0600

    Dynamic code

--- flask_integration/Get_CSV_File_Headers.py
+++ flask_integration/Get_CSV_File_Headers.py
@@ -5,18 +5,6 @@ def Get_CSV():
         # creating an object of csv reader
         # with the delimiter as ,
         csv_reader = csv.reader(csv_file, delimiter=',')
-
         headers = next(csv_reader)
-
-        # Print the headers
-        for header in headers:
-            print(header)
-        # Store the headers in a dictionary
-        header_dict = {index: header for index, header in enumerate(headers)}
-
-        # Alternatively, you can access headers dynamically using the index
-        index = 0
-        header_value = header_dict.get(index)  # Returns None if index is not found
-        if header_value:
-            print(f"Header at index {index}: {header_value}")
+        print(headers)
 Get_CSV()
--- flask_integration/get_model.py
+++ flask_integration/get_model.py
@@ -1,18 +1,26 @@
 import pickle
 import pandas as pd
-class LoadModel:
-    #Loading the model
-    def __init__(self,MODEL_PATH):
-        self.loaded_model = pickle.load(open(MODEL_PATH, 'rb'))
+import csv
 
-    def predict_class(self, Age,Bp,Sg,Al,Su,Rbc,Pc,Pcc,Ba,Bgr,Bu,Sc,Sod,Pot,Hemo,Pcv,Wbcc,Rbcc,Htn,Dm,Cad,Appet,pe,Ane):
-        # initialize list of lists 
-        data = [[Age,Bp,Sg,Al,Su,Rbc,Pc,Pcc,Ba,Bgr,Bu,Sc,Sod,Pot,Hemo,Pcv,Wbcc,Rbcc,Htn,Dm,Cad,Appet,pe,Ane]]
-        
-        # Create the pandas DataFrame 
-        df = pd.DataFrame(data, columns = ['Age', 'Bp', 'Sg', 'Al', 'Su', 'Rbc', 'Pc', 'Pcc', 'Ba','Bgr', 'Bu', 'Sc', 'Sod', 'Pot', 'Hemo', 'Pcv', 'Wbcc', 'Rbcc', 'Htn', 'Dm', 'Cad', 'Appet', 'pe', 'Ane'])
+class LoadModel:
+    def __init__(self, MODEL_PATH):
+        self.loaded_model = pickle.load(open(MODEL_PATH, 'rb'))
+        self.headers = self.get_headers()
+
+    def get_headers(self):
+        with open('../data/uploaded_file.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            headers = next(csv_reader)
+            if 'Class' in headers:
+                headers.remove('Class')
+            return headers
+
+    def predict_class(self, *variable_values):
+        data = [list(variable_values)]
+        df = pd.DataFrame(data, columns=self.headers)
         new_pred = self.loaded_model.predict(df)
         return new_pred
+
 #48,50,1.02,4,0,1,1,0,0,121,18,1.2,137.53,4.63,11.3,44,6000,4.71,1,1,0,1,0,0 -> for 1
 #82,60,1.025,0,0,1,1,0,0,137,17,0.4,147,4.7,14.3,34,6700,5.9,0,1,0,1,0,0 -> for 0
 
